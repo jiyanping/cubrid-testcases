1f474c82b8abcf2429f75c0657171b056164288e
jiyanping <jiyanping@navercorp.com>
2018-11-02 19:12:17 +0900

    [CUBRIDQA-807] add test cases for online index

---
+++ sql/_31_cherry/issue_21506_online_index/cases/cbrd_22421.sql
@@ -0,0 +1,15 @@
+drop table if exists t1;
+
+create table t1(a int,b int);
+
+insert into t1 values(1,1),(2,2),(3,3);
+
+create index idx1 on t1(a) with online;
+
+create index idx2 on t1(b);
+
+create unique index idx3 on t1(a,b) with online;
+
+select index_name,status from db_index where class_name = 't1';
+
+drop t1;
